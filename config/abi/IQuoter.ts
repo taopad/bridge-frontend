72ee11356e8e74ad11eace9c9ae697a51be4a535
niera26 <niera26@outlook.com>
2023-11-30 14:22:03 +0100

    add the quoter and default addresses

---
+++ config/abi/IQuoter.ts
@@ -0,0 +1,74 @@
+const abi = [
+    {
+        "inputs": [
+            {
+                "internalType": "bytes",
+                "name": "path",
+                "type": "bytes"
+            },
+            {
+                "internalType": "uint256",
+                "name": "amountIn",
+                "type": "uint256"
+            }
+        ],
+        "name": "quoteExactInput",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "amountOut",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "components": [
+                    {
+                        "internalType": "address",
+                        "name": "tokenIn",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "address",
+                        "name": "tokenOut",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "uint256",
+                        "name": "amountIn",
+                        "type": "uint256"
+                    },
+                    {
+                        "internalType": "uint24",
+                        "name": "fee",
+                        "type": "uint24"
+                    },
+                    {
+                        "internalType": "uint160",
+                        "name": "sqrtPriceLimitX96",
+                        "type": "uint160"
+                    }
+                ],
+                "internalType": "struct IUniswapV3StaticQuoter.QuoteExactInputSingleParams",
+                "name": "params",
+                "type": "tuple"
+            }
+        ],
+        "name": "quoteExactInputSingle",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "amountOut",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+] as const
+
+export default abi
--- config/contracts.ts
+++ config/contracts.ts
@@ -2,8 +2,10 @@ import NativeTokenAbi from "./abi/ERC20Rewards";
 import RewardTokenAbi from "./abi/IERC20Metadata";
 import CompounderAbi from "./abi/ERC20RewardsCompounder";
 import IUniswapV2RouterAbi from "./abi/IUniswapV2Router02";
+import IQuoterAbi from "./abi/IQuoter";
 
 if (!process.env.NEXT_PUBLIC_ROUTER_ADDRESS) throw Error("NEXT_PUBLIC_ROUTER_ADDRESS env variable must be set");
+if (!process.env.NEXT_PUBLIC_QUOTER_ADDRESS) throw Error("NEXT_PUBLIC_QUOTER_ADDRESS env variable must be set");
 if (!process.env.NEXT_PUBLIC_NATIVE_TOKEN_ADDRESS) throw Error("NEXT_PUBLIC_NATIVE_TOKEN_ADDRESS env variable must be set");
 if (!process.env.NEXT_PUBLIC_REWARD_TOKEN_ADDRESS) throw Error("NEXT_PUBLIC_REWARD_TOKEN_ADDRESS env variable must be set");
 if (!process.env.NEXT_PUBLIC_COMPOUNDER_ADDRESS) throw Error("NEXT_PUBLIC_COMPOUNDER_ADDRESS env variable must be set");
@@ -11,6 +13,11 @@ if (!process.env.NEXT_PUBLIC_COMPOUNDER_ADDRESS) throw Error("NEXT_PUBLIC_COMPOU
 export const RouterContract = {
     abi: IUniswapV2RouterAbi,
     address: process.env.NEXT_PUBLIC_ROUTER_ADDRESS as `0x${string}`,
+}
+
+export const QuoterContract = {
+    abi: IQuoterAbi,
+    address: process.env.NEXT_PUBLIC_QUOTER_ADDRESS as `0x${string}`,
 }
 
 export const NativeTokenContract = {
