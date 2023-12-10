50e54aa33db833f2aae1844195554979999321ea
niera26 <niera26@outlook.com>
2023-12-10 18:36:31 +0100

    dont event need intermediate eth value for apr

--- hooks/useApr.ts
+++ hooks/useApr.ts
@@ -5,7 +5,6 @@ import { useAppWatch } from "./useAppWatch";
 import { useAppStatic } from "./useAppStatic";
 
 const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
-const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
 
 const blocksPerDay = 7123n
 
@@ -29,11 +28,6 @@ export function useApr() {
                 functionName: "startBlock",
             },
             {
-                ...RouterContract,
-                functionName: "getAmountsOut",
-                args: [parseEther("1"), [WETH, USDC]],
-            },
-            {
                 ...QuoterContract,
                 functionName: "quoteExactInputSingle",
                 args: [{
@@ -47,29 +41,28 @@ export function useApr() {
             {
                 ...RouterContract,
                 functionName: "getAmountsOut",
-                args: [totalShares, [NativeTokenContract.address, WETH, USDC]],
+                args: [totalShares, [NativeTokenContract.address, WETH]],
             },
         ],
         enabled: appWatch.isSuccess && rewardDecimal > 0 && poolFee > 0 && totalShares > 0,
         select: (data) => {
             const currentBlockNumber = blockNumber.data ?? 0n
             const startBlock = data[0].result ?? 0n
-            const ethUsdcPrice = data[1].result?.[1] ?? 0n
-            const rewardEthPrice = data[2].result ?? 0n
-            const totalUsdcLocked = data[3].result?.[2] ?? 0n
+            const rewardEthPrice = data[1].result ?? 0n
+            const totalEthLocked = data[2].result?.[1] ?? 0n
 
-            if (totalUsdcLocked === 0n) return 0
+            if (totalEthLocked === 0n) return 0
 
             if (currentBlockNumber < startBlock) return 0
 
             const elapsedBlocks = currentBlockNumber - startBlock
             const ethDonated = rewardDonations * rewardEthPrice
             const ethDistributed = rewardDistributed * rewardEthPrice
-            const usdcDistributed = (ethCollected + ethDonated + ethDistributed) * ethUsdcPrice
-            const usdcPerBlock = usdcDistributed / elapsedBlocks
-            const usdcPerYear = usdcPerBlock * blocksPerDay * 365n
+            const totalDistributedEth = ethCollected + ethDonated + ethDistributed
+            const ethPerBlock = totalDistributedEth / elapsedBlocks
+            const ethPerYear = ethPerBlock * blocksPerDay * 365n
 
-            return usdcPerYear / (totalUsdcLocked * parseEther("1"))
+            return ethPerYear / totalEthLocked
         },
     })
 }
