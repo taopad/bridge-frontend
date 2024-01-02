f2c841a8fe7b7317f04322c584b777b6b3a0f9cf
niera26 <niera26@outlook.com>
2024-01-02 17:26:52 +0100

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import Link from "next/link";
 const myFont = localFont({ src: "./RocGroteskWideMedium.otf" })
 
 export const metadata: Metadata = {
-    title: 'TaoPad bridge',
-    description: 'TaoPad bridge for wTao',
+    title: 'Taobridge',
+    description: 'Taobridge (built by taopad.io)',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
