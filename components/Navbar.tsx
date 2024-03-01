9b08790711ee55f9449acc9daaa9bc9730d6ca2a
niera26 <niera26@outlook.com>
2024-03-01 18:59:31 +0100

    add aspect ratio for logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,12 @@
 import Link from "next/link"
-import Image from "next/image"
 import { WalletButton } from "@/components/WalletButton"
+import { NavbarLogo } from "./NavbarLogo"
 
 export function Navbar() {
     return (
         <div className="flex container mx-auto py-4 justify-between items-center">
-            <Link href="/" className="block relative w-32 h-16 lg:w-48">
-                <Image
-                    src="/TaoPadLogo.png"
-                    alt="TaoPad"
-                    fill
-                    sizes="1"
-                    style={{ objectFit: "contain" }}
-                />
+            <Link href="/" className="block w-48">
+                <NavbarLogo />
             </Link>
             <WalletButton />
         </div>
