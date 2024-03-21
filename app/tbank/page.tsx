9b7d16cbc4ec11457b6729b25602ef8535185010
niera26 <niera26@outlook.com>
2024-03-21 14:41:21 +0100

    add tbank cover

--- app/tbank/page.tsx
+++ app/tbank/page.tsx
@@ -1,14 +1,21 @@
+import Link from "next/link"
 import { BridgeLayout } from "@/components/BridgeLayout"
+import { TokenAddresses } from "@/components/TokenAddresses"
+import { TokenCoverImage } from "@/components/TokenCoverImage"
 import { BridgeCard } from "@/components/BridgeCard/BridgeCard"
 import { BridgeForm } from "@/components/BridgeCard/BridgeForm"
 import { NetworkCard } from "@/components/NetworkCard/NetworkCard"
-import { TokenAddresses } from "@/components/TokenAddresses"
 
 const name = "TBANK"
+const appUrl = "https://app.taobank.ai"
+const coverUrl = "/covers/tbank.png"
 
 export default function wTao() {
     return (
         <BridgeLayout token="tbank">
+            <Link href={appUrl} target="_blank">
+                <TokenCoverImage url={coverUrl} />
+            </Link>
             <NetworkCard />
             <BridgeCard name={name}>
                 <BridgeForm version="v2" />
